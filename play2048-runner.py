7454f32931b45ecd14f4dbc64050161e6499a15b
Jayanthi Challa <76255327+Jayanthi-Challa@users.noreply.github.com>
2021-02-12 10:30:18 +0530

    testing

---
+++ play2048-runner.py
@@ -0,0 +1,8 @@
+# Python
+#you do not need to edit this file, it is the runner file
+
+#this line imports your code from play2048.py
+import play2048
+
+#this line runs the play2048 function with the parameter 100, you can change this parameter if you'd like to test your program with different iterations
+play2048.play2048(100)
